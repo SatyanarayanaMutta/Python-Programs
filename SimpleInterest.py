5274e2e3f2b068f294faa530cd945bb2472ad152
Shaik Badulla <skbadulla809@gmail.com>
2019-04-17 11:20:15 +0530

    Simple Interest

---
+++ SimpleInterest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Simple Interest
+p=float(input("Enter the Principal :"))
+t=float(input("Enter the Rate of interest :"))
+r=float(input("Enter the Time period (In Months):"))
+print("Simple Interest is :",(r * t * p) / 100)
