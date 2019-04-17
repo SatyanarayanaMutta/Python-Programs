edac6ed61a86e2bfb8b17963b136ddc863442b5e
Shaik Badulla <skbadulla809@gmail.com>
2019-04-17 10:17:07 +0530

    Update MaxOf3.py

--- MaxOf3.py
+++ MaxOf3.py
@@ -1,1 +1,20 @@
 
+#!/usr/bin/env python3
+# Initioalize values
+a,b,c=500,15,100
+
+# If you want to prompt(take) values from user then use input() method
+"""
+a=int(input("Enter a value: "))
+b=int(input("Enter b value: "))
+c=int(input("Enter c value: "))
+# int() is a method using for type conversion
+"""
+# Comparing a with b and c
+if ((a>b) and (a>c)):
+    print("%s is big" %a)
+# Comparing b and c
+elif(b<c):
+    print("%s is big" %c)
+else:
+    print("%s is big" %b)
